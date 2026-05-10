6b25894ce258dba41a2cc40cc58d704d48a3bb5a
wowito68 <memohalo12@hotmail.com>
2026-05-09 21:39:52 -0600

    navegacion principal

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/reglas')
+def reglas():
+    return render_template('reglas.html')
+
+@app.route('/notas')
+def notas():
+    return render_template('notas.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/tiempo')
+def tiempo():
+    return render_template('tiempo.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
